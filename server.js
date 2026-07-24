be499c186ef91bc336be60c81f1ae41a626ec959
NguemaMadja <nguemamadja@gmail.com>
2026-07-24 20:31:02 +0200

    Add test route and use environment port
    Added a test route and updated server port configuration.

--- server.js
+++ server.js
@@ -569,11 +569,16 @@ app.delete('/profesores/:id', async (req, res) => {
   }
 });
 
-
-
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('SmartCampusGuide funcionando en Render 🚀');
+});
 
 // ---------------- INICIO SERVIDOR ----------------
-const PORT = 4000;
+// Render asigna el puerto automáticamente
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
+
